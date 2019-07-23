951557dab31abdb16b910974d64abd0174d66cd0
Wator Vapor <watovapor@gmail.com>
2019-07-23 12:41:31 +0000

    news pumper

--- wordtag/app.js
+++ wordtag/app.js
@@ -30,10 +30,34 @@ const onDiscoveryNewLink = (href) => {
   });
 }
 
+
 const onNewsText = (txt,href) => {
-  console.log('onNewsText::txt=<',txt,'>');
-  console.log('onNewsText::href=<',href,'>');
-  wai.article(txt);
+  //console.log('onNewsText::txt=<',txt,'>');
+  //console.log('onNewsText::href=<',href,'>');
+  let tags = wai.article(txt);
+  //console.log('onNewsText::tags=<',tags,'>');
+  postTwitter(txt,href,tags);
+}
+
+const Twitter = require('twitter');
+const clientTwitter = new Twitter({
+  consumer_key: 'cKcYfmC6niawGY7kLC9hGHafW',
+  consumer_secret: 'MZ5FP6BYItZOOLuciabtuwHyiHH78bqRb1HnrE3K4PK9obDM5l',
+  access_token_key: '2479678550-zBgOIqB81GtIQj99K6pyqbDlN0dqlJx8pbNCnVp',
+  access_token_secret: '7gxQyigISdvfjKNgQA6VCEIOtcZMSWDyGQs04rg2NgXXq'
+});
+const postTwitter = (txt,href,tags) => {
+  console.log('postTwitter::txt=<',txt,'>');
+  console.log('postTwitter::href=<',href,'>');
+  console.log('postTwitter::tags=<',tags,'>');
+  const params = {screen_name: 'nodejs'};
+  clientTwitter.get('statuses/user_timeline', params, (error, tweets, response) =>{
+    if (error) {
+      throw error;
+    }
+    console.log('postTwitter::tweets=<',tweets,'>');
+    console.log('postTwitter::response=<',response,'>');
+  });
 }
 
 /**
--- wordtag/wai/wai.tagbot.js
+++ wordtag/wai/wai.tagbot.js
@@ -25,12 +25,18 @@ class WaiTagBot extends WaiBase {
     })
   }
   article(doc) {
+    this.wordFreqs_ = {};
     this.tbdWords_ = {};
     super.article(doc,this.onSentence_.bind(this));
+    //console.log('WaiTagBot::article this.tbdWords_=<',this.tbdWords_,'>');
+    //console.log('WaiTagBot::article this.wordFreqs_=<',this.wordFreqs_,'>');
+    const pureCollect = super.FilterOutInside_(this.wordFreqs_);
+    //console.log('WaiTagBot::article pureCollect=<',pureCollect,'>');
+    return this.calcWeight_(pureCollect);
   } 
   //
   onSentence_(sentence) {
-    console.log('WaiTagBot::onSentence_ sentence=<',sentence,'>');
+    //console.log('WaiTagBot::onSentence_ sentence=<',sentence,'>');
     for(let i = 0 ;i < sentence.length;i++) {
       let utf8 = sentence[i];
       //console.log('WaiTagBot::onSentence_ utf8=<',utf8,'>');
@@ -50,11 +56,40 @@ class WaiTagBot extends WaiBase {
           let word = concat.join('');
           //console.log('WaiTagBot::onSentence_ word=<',word,'>');
           if(this.phrase_[word]) {
-            console.log('WaiTagBot::onSentence_ word=<',word,'>');
+            //console.log('WaiTagBot::onSentence_ word=<',word,'>');
+            this.tbdWords_[word] = this.phrase_[word];
+            if(this.wordFreqs_[word]) {
+              this.wordFreqs_[word]++;
+            } else {
+              this.wordFreqs_[word] = 1;
+            }
           }
         }
       }
     }
+  }
+  
+  calcWeight_(collect) {
+    let weights = [];
+    for(let word in collect) {
+      //console.log('WaiTagBot::calcWeight_ word=<',word.length,'>');
+      let probability = this.phrase_[word];
+      let freq = collect[word];
+      //console.log('WaiTagBot::calcWeight_ freq=<',freq,'>');
+      //sortedCollect.push({w:word,freq:collect[word]})
+      let weight = freq * freq * freq * probability * word.length * word.length;
+      weights.push({tag:word,weight:weight});
+    }
+    //console.log('WaiTagBot::calcWeight_ weights=<',weights,'>');
+    weights.sort((a,b) => {
+      //console.log('WaiTagBot::calcWeight_ a=<',a,'>');
+      //console.log('WaiTagBot::calcWeight_ b=<',b,'>');
+      if(a.weight > b.weight) return -1;
+      if(a.weight < b.weight) return 1;
+      return 0;
+    });
+    //console.log('WaiTagBot::calcWeight_ weights=<',weights,'>');
+    return weights.slice(0, 16);
   }
 }
 

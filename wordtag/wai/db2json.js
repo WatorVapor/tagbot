a3b5487d381f6f767839fc0ed10bdfbce842c94e
Wator Vapor <watorvapor@gmail.com>
2019-07-25 17:17:48 +0900

    Update db2json.js

--- wordtag/wai/db2json.js
+++ wordtag/wai/db2json.js
@@ -1,1 +1,18 @@
+const level = require('level');
+const gDBPath = '/watorvapor/wai.storage/wai.native.wator/db/zhizi/cn.cast';
+const db_ = level(gDBPath,{ createIfMissing: false });
+const phrase_ = {};
+db_.createReadStream()
+.on('data', (data) =>{
+  //console.log('WaiTagBot::onSentence_ data=<',data,'>');
+  phrase_[data.key] = parseFloat(data.value);
+})
+.on('error', (err) =>{
+})
+.on('close', () =>{
+})
+.on('end', (evt) =>{
+  console.log('WaiTagBot::constructor end evt=<',evt,'>');
+});
+
 

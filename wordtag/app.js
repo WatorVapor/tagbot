f300a3f60aa94774442f16081ebdc2246c3f69d0
Wator Vapor <watorvapor@gmail.com>
2019-07-25 17:04:25 +0900

    Update app.js

--- wordtag/app.js
+++ wordtag/app.js
@@ -31,7 +31,7 @@ const onDiscoveryNewLink = (href) => {
   if(escape < 1000 * 60){
     console.log('onDiscoveryNewLink:: too busy href=<',href,'>');
     console.log('onDiscoveryNewLink:: escape=<',escape,'>');
-    console.log('onDiscoveryNewLink:: now=<',now.toGMTString(),'>');
+    console.log('onDiscoveryNewLink:: now=<',now.toUTCString(),'>');
     return;
   }
   lastPostTime = now;

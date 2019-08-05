9c7c95129da5a239017d00bee3c95124c5b2ea41
Wator Vapor <watovapor@gmail.com>
2019-08-05 11:53:05 +0000

    news pumper

--- wordtag/appML.js
+++ wordtag/appML.js
@@ -1,4 +1,2 @@
-//const strConstTextDBPath = '/watorvapor/ldfs/tagbot/ja/news_text_db';
-//const strConstDiscoveryPath = '/watorvapor/ldfs/tagbot/ja/news_discovery_db';
 const TagBot = require('./tagbot.js');
 const bot = new TagBot();

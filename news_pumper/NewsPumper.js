0be4e9c44d18c96c3cf2148c1195e071f6698e00
Wator Vapor <watorvapor@gmail.com>
2019-09-18 11:23:09 +0900

    Update NewsPumper.js

--- news_pumper/NewsPumper.js
+++ news_pumper/NewsPumper.js
@@ -61,7 +61,7 @@ module.exports = class NewsPumper {
         body += chunk;
       });
       resp.on('end', () => {
-        console.log('readNews_::resp.headers=<',resp.headers,'>');
+        console.log('readNews_::resp.rawHeaders=<',resp.rawHeaders,'>');
         self.onHttpBody_(body);
       });      
     }).on("error", (err) => {
